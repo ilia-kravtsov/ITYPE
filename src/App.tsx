a4bf16d36658445aac7edd64267ed8dd2fb042b8
ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-10-25 19:49:52 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,29 @@ export type TaskType = {
 
 export type FilterValuesType = 'all' | 'active' | 'completed'
 
+/*
+Create
+Read => part, pagination, filtration, sort
+Update
+Delete removeTask
+*/
+
 function App() {
 
     // BLL: (Здесь храним все данные, которые будем закидывать в TodoList) Данные:
     const todoListTitle: string = "What to learn"
-
     const [tasksForTodoList, setTasksForTodoList] = useState<Array<TaskType>>([
         {id: 1, title: "HTML & CSS", isDone: true},
         {id: 2, title: "JS & ES6", isDone: true},
         {id: 3, title: "REACT & TS", isDone: false},
     ])
-
     const removeTask = (taskId: number) => {
         setTasksForTodoList (tasksForTodoList.filter(task => task.id !== taskId))
     }
 
 
-    const [filter, setFilter] = useState<FilterValuesType>('all')
 
+    const [filter, setFilter] = useState<FilterValuesType>('all')
     const changeFilter = (filter: FilterValuesType) => {
         setFilter(filter)
     }
